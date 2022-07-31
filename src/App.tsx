29a53e845c2dfd39fc3b2dec6516bddf808d954f
Josh Gray <jdgray.dev@gmail.com>
2022-07-30 18:11:05 -0700

    login inputs + logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import logo from './logo.svg';
-import './App.css';
+
 import AppRoutes from './routes/AppRoutes';
 import { useGetDogByIdQuery } from './redux/dog/createApi';
 
@@ -8,21 +7,7 @@ function App() {
   console.log('data', data);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>🐶 Welcome to Sniffr! 🐶</h2>
-        <a
-          className="App-link"
-          href="https://github.com/the-best-team-seven/sniffr-fe"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          View the project on GitHub
-        </a>
-        <AppRoutes />
-      </header>
-    </div>
+    <AppRoutes/>
   );
 }
 
--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,8 +1,17 @@
+import { Box, Button, TextField } from "@mui/material";
+import logo from "../../assets/logo/logo.svg";
+
+
 const LoginPage: React.FC = (): React.ReactElement => {
   return (
-    <div>
-      <h3>Login Page, duh</h3>
-    </div>
+    <Box>
+      <Box>
+        <img src={logo} alt={"logo"} />
+      </Box>
+      <TextField label={"Username"} variant={"outlined"} /> 
+      <TextField label={"Password"} variant={"outlined"} /> 
+      <Button  variant={"contained"} >Login</Button>
+    </Box>
   );
 };
 
