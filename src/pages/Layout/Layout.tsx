6d9c9ed26ad8442d512533590cec882ffa643483
Allie Robinson <48700332+allie-rae@users.noreply.github.com>
2022-08-18 17:43:08 -0500

    Update src/pages/Layout/Layout.tsx

--- src/pages/Layout/Layout.tsx
+++ src/pages/Layout/Layout.tsx
@@ -39,7 +39,9 @@ const Layout = (props: any) => {
 
   const drawer = (
     <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
-      <Typography variant="h6" sx={{ my: 2, cursor: 'pointer' }} onClick={() => navigate('/')}>
+      <Box sx={{ cursor: 'pointer' }}>
+          <Typography variant="h6" sx={{ my: 2 }} onClick={() => navigate('/')}>
+      </Box>
         sniffr
       </Typography>
       <Divider />
