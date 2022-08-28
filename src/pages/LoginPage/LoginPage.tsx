5d488ff3cfc03849f208c9b903f99b3ade02f32a
Allie Robinson <allierob93@gmail.com>
2022-08-27 20:49:47 -0500

    start on react-hooks-form

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,9 +1,21 @@
 import { Box, Button, Link, TextField } from '@mui/material';
+import { useForm } from 'react-hook-form';
 import { Link as RouterLink } from 'react-router-dom';
 
 import logo from '../../assets/logo/logo.svg';
 
+interface LoginInputs {
+  username: string;
+  password: string;
+}
+
 const LoginPage: React.FC = (): React.ReactElement => {
+  const { register } = useForm<LoginInputs>({
+    defaultValues: {
+      password: '',
+      username: '', 
+    }
+  });
   return (
     <Box
       sx={{
@@ -24,12 +36,14 @@ const LoginPage: React.FC = (): React.ReactElement => {
         <TextField
           label="Username"
           variant="outlined"
+          value={username}
           sx={{ mb: 2, width: '100%' }}
         />
         <TextField
           label="Password"
           type="password"
           variant="outlined"
+          value={password}
           sx={{ mb: 2, width: '100%' }}
         />
         <Button variant="contained" fullWidth size="large" sx={{ mb: 2 }}>
